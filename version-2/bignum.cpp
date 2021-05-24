1933341933b9a156c6f3e70dcbc6af5a10d0c511
fcalonso <72171858+fcalonso@users.noreply.github.com>
2021-05-24 00:32:01 -0300

    Modificaciones de estructura y nuevas funciones

--- version-2/bignum.cpp
+++ version-2/bignum.cpp
@@ -6,23 +6,110 @@
 
 #include "bignum.h"
 
+string DiccionarioOperaciones(OPERACIONES);
+string DiccionarioDigitos("0123456789");
+
+unsigned short *_crear_digitos(unsigned precision)
+{
+    return precision ? new unsigned short[precision]: NULL;
+}
 bignum::bignum(unsigned precision=0)
 {
-    _digitos= precision ? new unsigned short(precision): NULL;
-    _precision=precision;
-    _largo=0;
-    _estado= OK;
+    _digitos = _crear_digitos(precision);
+    _precision = precision;
+    _largo = 0;
+    _signo = POSITIVO;
+    _estado = OK;
 
 }
 
 bignum::bignum(const bignum &original)
 {
-    _precision=original._precision;
-    _largo=original._largo;
-    _digitos=new unsigned short(_precision);
+    _precision = original._precision;
+    _largo = original._largo;
+    _digitos = new unsigned short[_precision];
+    _signo = original._signo;
 
     for(int i=0; i<_largo;i++)
         _digitos[i]=original._digitos[i];
     _estado=OK;
 }
 
+bignum::bignum(const string &linea, size_t precision)
+{
+    
+     _digitos = _crear_digitos(precision);
+     _precision = precision;
+
+     *this = linea;    
+
+}
+
+bignum::~bignum()
+{
+    if(_digitos!=NULL)
+        delete [] _digitos;
+    
+}
+
+size_t bignum::precision() const
+{
+    return _precision;
+}
+
+size_t bignum::largo() const
+{
+    return _largo;
+}
+
+signo_t bignum::signo() const
+{
+    return _signo;
+}
+
+status_t bignum::estado() const
+{
+    return _estado;
+}
+
+bool bignum::good() const
+{
+    return (_estado == OK)? true : false;
+}
+
+bignum &bignum::operator=(const bignum &original)
+{
+    if(this != &original)
+    {
+        if(_precision != original._precision)
+        {
+            delete [] _digitos;
+            _precision = original._precision;
+            _digitos= new unsigned short[_precision];
+        }
+        for(unsigned i=0; i< original._largo; i++)
+            _digitos[i]=original._digitos[i];
+        _largo=original._largo;
+    }
+    return *this;
+}
+
+bignum &bignum::operator=(const string &linea)
+{
+    string aux(linea);
+    size_t longitud = stringDigits(aux);
+
+    if( !longitud )   
+         _estado = ERROR_DIGITOS;
+    else if(  longitud > _precision )
+        _estado = ERROR_PRECISION;
+    else {
+        _estado = OK; 
+        _signo = ( aux[0] == '+')? POSITIVO : NEGATIVO;
+        _largo = longitud -1 ;
+        for(int i=longitud, j =0 ; i<0 ; --i , ++j)
+            _digitos[j]= aux[i];
+        
+        }
+
+}
--- version-2/bignum.h
+++ version-2/bignum.h
@@ -3,38 +3,64 @@
 
 #include <iostream>
 #include <string>
+#include <cctype>
 #include "errors.h"
+#include "utils.h"
+using namespace std;
 
 typedef enum status{
     OK,
+    NOK,
     ERROR_DIGITOS,
     ERROR_PRECISION,
 
 } status_t;
 
+ typedef enum signo{
+     POSITIVO,
+     NEGATIVO
+ } signo_t;
+
+
+#define OPERACIONES { \
+         '+', '-', '*'\
+         }
+
+enum operaciones {
+    SUMA,
+    RESTA,
+    MULTIPLICACION
+};
+
+
 class bignum
 {
     private:
         unsigned short *_digitos;
-        unsigned _largo;
-        unsigned _precision;
+        size_t _largo;
+        size_t _precision;
+        signo_t _signo;
         status_t _estado;
 
+        unsigned short *_crear_digitos(unsigned precision);
+        
     public:
         bignum(unsigned precision);
         bignum(const bignum &original);
+        bignum(const string &linea, size_t precision);
         ~bignum();
 
-        short precision() const;
-        short largo() const;
+        size_t precision() const;
+        size_t largo() const;
+        signo_t signo() const;
         status_t estado() const;
         bool good() const;
 
-        bignum operator=(const bignum &original);
-        bignum operator=(const string &linea);
-        friend bignum operator+(const bignum &sumando1 , const bignum &sumando2);
-        friend bignum operator-(const bignum &minuendo, const bignum &sustraendo);
-        friend bignum operator*(const bignum &factor1, const bignum &factor2);
+        bignum &operator=(const bignum &original);
+        bignum &operator=(const string &linea) ;
+        friend bignum &operator+(const bignum &sumando1 , const bignum &sumando2);
+        friend bignum &operator-(const bignum &minuendo, const bignum &sustraendo);
+        friend bignum &operator*(const bignum &factor1, const bignum &factor2);
         friend std::ostream& operator<<(std::ostream& ,const bignum&);
         friend std::istream& operator>>(std::istream& , const bignum&);
 };
